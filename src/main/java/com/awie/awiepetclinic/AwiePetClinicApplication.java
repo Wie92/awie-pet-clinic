b8850659d628ec899aa0b21be79c6bfe196fd79d
Hendry Susanto <35400792+HotWie@users.noreply.github.com>
2019-01-11 08:31:47 +0700

    Init Project

---
+++ src/main/java/com/awie/awiepetclinic/AwiePetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.awie.awiepetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwiePetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwiePetClinicApplication.class, args);
+	}
+
+}
+
