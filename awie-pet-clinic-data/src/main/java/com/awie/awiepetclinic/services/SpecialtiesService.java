309cf8830a371bd59900a4751bf734347f49fa8d
hendry <hendry@dell-5570>
2019-02-05 19:37:34 +0700

    Added Specialties Service Map Impl. Closes #31.

---
+++ awie-pet-clinic-data/src/main/java/com/awie/awiepetclinic/services/SpecialtiesService.java
@@ -0,0 +1,9 @@
+package com.awie.awiepetclinic.services;
+
+import com.awie.awiepetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long>{
+
+	
+	
+}
---
+++ awie-pet-clinic-data/src/main/java/com/awie/awiepetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,38 @@
+package com.awie.awiepetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.awie.awiepetclinic.model.Speciality;
+import com.awie.awiepetclinic.services.SpecialtiesService;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialtiesService{
+
+	@Override
+	public Set<Speciality> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Speciality findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Speciality save(Speciality object) {
+		return super.save(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Speciality object) {
+		super.delete(object);
+	}
+
+}
