86bc783bd6c0f01de998d4dadead011b5efe95ae
hendry <hendry@dell-5570>
2019-02-05 08:37:33 +0700

    Add Pets and Contacts to Owners on startup. Closes #33

--- awie-pet-clinic-data/src/main/java/com/awie/awiepetclinic/services/map/OwnerServiceMap.java
+++ awie-pet-clinic-data/src/main/java/com/awie/awiepetclinic/services/map/OwnerServiceMap.java
@@ -5,10 +5,21 @@ import java.util.Set;
 import org.springframework.stereotype.Service;
 
 import com.awie.awiepetclinic.model.Owner;
+import com.awie.awiepetclinic.model.Pet;
 import com.awie.awiepetclinic.services.OwnerService;
+import com.awie.awiepetclinic.services.PetService;
+import com.awie.awiepetclinic.services.PetTypeService;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService{
+
+	private final PetTypeService petTypeService;
+	private final PetService petService;
+	
+	public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+		this.petTypeService = petTypeService;
+		this.petService = petService;
+	}
 
 	@Override
 	public Set<Owner> findAll() {
@@ -22,7 +33,30 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
 	@Override
 	public Owner save(Owner owner) {
-		return super.save(owner);
+		
+		if(owner != null) {
+			if(owner.getPets() != null) {
+				owner.getPets().forEach(pet -> {
+					if(pet.getPetType() != null) {	
+						if(pet.getPetType().getId() == null) {
+							pet.setPetType(petTypeService.save(pet.getPetType()));
+						}
+					}else {
+						throw new RuntimeException("Pet Type is Required.");
+					}
+					
+					if(pet.getId() == null) {
+						Pet savedPet = petService.save(pet);
+						pet.setId(savedPet.getId());
+					}
+				});
+			}
+			
+			return super.save(owner);
+		}else {
+			return null;
+		}
+	
 	}
 	
 	@Override
--- awie-pet-clinic-web/src/main/java/com/awie/awiepetclinic/bootstrap/DataLoader.java
+++ awie-pet-clinic-web/src/main/java/com/awie/awiepetclinic/bootstrap/DataLoader.java
@@ -1,9 +1,13 @@
 package com.awie.awiepetclinic.bootstrap;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.awie.awiepetclinic.model.Owner;
+import com.awie.awiepetclinic.model.Pet;
 import com.awie.awiepetclinic.model.PetType;
 import com.awie.awiepetclinic.model.Vet;
 import com.awie.awiepetclinic.services.OwnerService;
@@ -43,13 +47,33 @@ public class DataLoader implements CommandLineRunner{
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Michael");
 		owner1.setLastName("Weston");
+		owner1.setAddress("Gang Keluarga 39Z");
+		owner1.setCity("Jakarta");
+		owner1.setTelephone("06212345678");
+		
+		Pet mikesPet = new Pet();
+		mikesPet.setName("Rosco");
+		mikesPet.setOwner(owner1);
+		mikesPet.setBirthDate(LocalDate.now());
+		mikesPet.setPetType(savedDogPetType);
+		owner1.getPets().add(mikesPet);
 		
 		ownerService.save(owner1);
 		
 		Owner owner2 = new Owner();
 		owner2.setFirstName("Fiona");
 		owner2.setLastName("Glenanne");
+		owner2.setAddress("Gang Keluarga 39Z");
+		owner2.setCity("Jakarta");
+		owner2.setTelephone("06212345678");
 		
+		Pet fionasPet = new Pet();
+		fionasPet.setName("Kitty");
+		fionasPet.setOwner(owner2);
+		fionasPet.setBirthDate(LocalDate.now());
+		fionasPet.setPetType(savedCatPetType);
+		owner2.getPets().add(fionasPet);
+			
 		ownerService.save(owner2);
 		
 		System.out.println("Loades Owners...");
