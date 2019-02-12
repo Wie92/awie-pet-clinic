6993b60a9d66f8b27496849441ed15beb1683001
hendry <hendry@dell-5570>
2019-02-12 08:06:30 +0700

    Added Project Lombok to Project. Closes #51.

--- awie-pet-clinic-data/src/main/java/com/awie/awiepetclinic/model/Speciality.java
+++ awie-pet-clinic-data/src/main/java/com/awie/awiepetclinic/model/Speciality.java
@@ -1,9 +1,24 @@
 package com.awie.awiepetclinic.model;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import com.awie.awiepetclinic.model.Owner.OwnerBuilder;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "specialities")
 public class Speciality extends BaseEntity {
@@ -11,11 +26,4 @@ public class Speciality extends BaseEntity {
 	@Column(name = "description")
 	private String description;
 
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
 }
